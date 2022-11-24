d33f772fb18b44d3e6a2d7391909c7ad8fb780b6
calvinandhika <calvin.andhika@gmail.com>
2022-11-24 11:07:39 +0700

    feat(project): first commit
    first commit to the Accurate test

---
+++ android/app/src/main/kotlin/com/calvinandhika/accurate_test/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.calvinandhika.accurate_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants/config.dart
@@ -0,0 +1,8 @@
+// ignore_for_file: constant_identifier_names
+
+class GlobalConfig {
+  static const BASE_URL =
+      'https://627e360ab75a25d3f3b37d5a.mockapi.io/api/v1/accurate/user';
+  static const CITY_API_URL =
+      'https://627e360ab75a25d3f3b37d5a.mockapi.io/api/v1/accurate/city';
+}
